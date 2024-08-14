712f1a8e5e364ea1d67dddb07a822bc3ef11e632
kyoohyunlee <khyhlee@naver.com>
2024-08-14 19:13:40 +0900

    8/14 ArrayList

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        int[] array = new int[5];
+        array[0]=1;
+        array[1]=2;
+        array[2]=3;
+        System.out.println(array);
+        System.out.println(Arrays.toString(array));
     }
+
+
 }
