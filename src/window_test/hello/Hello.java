de465ccdd908af1ab4cfbad7aa9b5761e958777f
kyoohyunlee <khyhlee@naver.com>
2024-11-22 20:47:22 +0900

    dev: windowtest HelloJava

---
+++ src/window_test/hello/Hello.java
@@ -0,0 +1,12 @@
+package window_test.hello;
+
+public class Hello {
+    public static void main(String[] args) {
+        String m = HelloJava();
+        System.out.println(m);
+    }
+
+    public static String HelloJava(){
+        return "HelloJava";
+    }
+}
